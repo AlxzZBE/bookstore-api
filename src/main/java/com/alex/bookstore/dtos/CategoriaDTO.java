fba40856696ad596c879f1df25475cf7141312d9
AlxzZBE <alexsander_lima99@hotmail.com>
2022-02-07 21:08:10 -0300

    findAll para categoria utilizando DTO

---
+++ src/main/java/com/alex/bookstore/dtos/CategoriaDTO.java
@@ -0,0 +1,28 @@
+package com.alex.bookstore.dtos;
+
+import com.alex.bookstore.domain.Categoria;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class CategoriaDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String desc;
+
+    public CategoriaDTO() {
+        super();
+    }
+
+    public CategoriaDTO(Categoria obj) {
+        this.id = obj.getId();
+        this.name = obj.getName();
+        this.desc = obj.getDesc();
+    }
+}
